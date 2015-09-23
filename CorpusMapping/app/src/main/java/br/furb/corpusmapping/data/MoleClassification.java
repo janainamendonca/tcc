5868ca5d5694dd66934cde3b873f01931c47f3cf
Janaína Mendonça <janaezico@gmail.com>
2015-09-22 23:39:21 -0300

    Colocado o id do recurso e a descrição em cada classificação

--- CorpusMapping/app/src/main/java/br/furb/corpusmapping/data/MoleClassification.java
+++ CorpusMapping/app/src/main/java/br/furb/corpusmapping/data/MoleClassification.java
@@ -1,15 +1,31 @@
 package br.furb.corpusmapping.data;
 
+import br.furb.corpusmapping.R;
+
 /**
  * Created by Janaina on 11/09/2015.
  */
 public enum MoleClassification {
 
-    NORMAL,
+    NORMAL(R.drawable.ic_class_green, "Normal"),
 
-    ATTENTION,
+    ATTENTION(R.drawable.ic_class_yellow, "Atenção"),
 
-    DANGER;
+    DANGER(R.drawable.ic_class_red, "Perigo");
 
+    private int resource;
+    private String description;
 
+    private MoleClassification(int resource, String description){
+        this.resource = resource;
+        this.description = description;
+    }
+
+    public int getResource() {
+        return resource;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
